a9b4d95094b3061a81865f82fc41a45e5b376d11
VAISHNAVI-R-V <vaishucv14@gmail.com>
2021-09-17 03:39:58 +0530

    ADDED: Welcome message

---
+++ src/main/java/com/moodanayzer/MoodAnalyzer.java
@@ -0,0 +1,15 @@
+package com.moodanayzer;
+
+/**
+ * @author : VAISHNAVI R. VISHWAKRMA.
+ * @purpose: To Print Welcome message.
+ * @SINCE 15-09-2021.
+ */
+
+public class MoodAnalyzer {
+    // main method
+    public static void main(String[] args) {
+        // printing welcome message.
+        System.out.println("Welcome to Mood Analyzer Program");
+    }
+}
